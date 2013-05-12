7977fb850287615a96baaecffc9d1dcb8a8b0157
Friedrich Fell <friedrich.fell87@gmail.com>
2013-05-12 18:26:35 +0200

    added Übung 3 -4

--- Übung_3/main.c
+++ Übung_3/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+
 
 void strcpy(char *s, char *t){
 	while((*t++=*s++) != '\0'){
@@ -16,18 +18,59 @@ int strcmp(char *s, char *t){
 }
 
 void *memcpy(void *s, const void *ct, size_t n){
+	void *temp = s;
+	while(n--){
+		*(char *)s = *(char *)ct;
+		s = (char *)s + 1;
+		ct = (char *)ct +1;
+	}
+	return temp;
+}
+
+void sleep(int seconds){
+	time_t startTime;
+	time_t actualTime;
+	time_t finishTime;
+	struct tm endTime;
+	double diff;
+	int i;
+
+	actualTime = time(&startTime);
+	endTime = *localtime(&startTime);
+	
+	endTime.tm_sec += seconds;
+
+	finishTime = mktime(&endTime);
+
+	for(i = 0; ; i++){
+		actualTime = time(&startTime);
+		if(i%100 == 1){
+			diff = difftime(finishTime,actualTime);
+			if(diff <= 0){
+				return;
+			}
+		}
+	}
 
 }
 
 void main(){
 	char string [] = "Hallo";
 	char ausgabe [] = "Hallo";
+	char test[3];
+	char *ptr = 0;
 
 	//strcpy(string, ausgabe);
-	int erg = strcmp(string, ausgabe);
-
+	//int erg = strcmp(string, ausgabe);
+	//ptr = (char *)memcpy(test, string, 3);
+	while(1){
+		printf("Warte 3 sekunden");
+		sleep(3);
+		printf("\a\n");
+	}
+	//printf("Ausgabe: %s", ptr);
 	//printf("Ausgabe: %s", ausgabe);
-	printf("Compare: %i", erg);
+	//printf("Compare: %i", erg);
 
 	getchar();
 }
